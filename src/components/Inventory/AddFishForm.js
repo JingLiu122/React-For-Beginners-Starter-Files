a8ee3a9f1d00a4d7f9e0d9c72114dc68fda961bd
Sean Liu <jingfengliu231@gmail.com>
2020-05-29 16:51:13 -0400

    add AddFishForm component, Router component
    add a Router wrapper component with React Router; add AddFishForm component: form data is retrieving by refs, then lifting them up to the higher-level component state with event handlers.

---
+++ src/components/Inventory/AddFishForm.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = e => {
+    e.preventDefault();
+    const fish = {
+      name: this.nameRef.current.value,
+      price: parseFloat(this.priceRef.current.value),
+      status: this.statusRef.current.value,
+      desc: this.descRef.current.value,
+      image: this.imageRef.current.value
+    };
+    this.props.addFish(fish);
+
+    // reset the form
+    e.currentTarget.parentNode.reset();
+  };
+
+  render() {
+    return (
+      <form className="fish-edit">
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
+        <input
+          name="price"
+          ref={this.priceRef}
+          type="text"
+          placeholder="Price"
+        />
+        <select name="status" ref={this.statusRef}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea name="desc" ref={this.descRef} placeholder="Description" />
+        <input name="image" ref={this.imageRef} placeholder="Image" />
+        <button type="Submit" onClick={this.createFish}>
+          + Add
+        </button>
+      </form>
+    );
+  }
+}
